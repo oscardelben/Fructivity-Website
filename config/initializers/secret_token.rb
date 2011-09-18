06477d3c45848e0d5000673206faad8993586903
Oscar Del Ben <info@oscardelben.com>
2011-09-18 18:41:38 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fructivity::Application.config.secret_token = '5bb00ec4cd985a62a314e70eda8db1d882ce85ed2926e39a2702f2da07182e7678b0c169f945371b665088e7fc5fbe54ba7e42512b1fbe0983951282812f67b2'
